ce7640534e75b706f083189c8a821c43c07ee315
Hrvoje Zeljko (hrvoje.zeljko) <hrvoje.zeljko@e-ito.hr>
2017-08-09 09:43:31 +0200

    Root password when setting access to mysql. Reorganization of mysql commands in admin script.

--- Admin/filesystem/mysql_data/set_access.sh
+++ Admin/filesystem/mysql_data/set_access.sh
@@ -2,15 +2,17 @@
 
 HOST="localhost"
 PORT="3306"
+PASSWORD=""
 #
-while getopts h:p: OPT; do
+while getopts h:p:b: OPT; do
 	[ $OPT == "h" ] && HOST=$OPTARG
 	[ $OPT == "p" ] && PORT=$OPTARG
+    [ $OPT == "b" ] && PASSWORD=$OPTARG
 done
 #
 shift $((OPTIND-1))
 
 # Set the MySQL databases user access
 echo -n "Setting the MySQL databases user access..."
-mysql -h $HOST -P $PORT -w < /mysql_data/sql_scripts/import_access.sql
+mysql -h $HOST -P $PORT -w --password=$PASSWORD < /mysql_data/sql_scripts/import_access.sql
 echo "done."
