88cb420233f1a3a357ba6cfa84cca4555abe6831
Hrvoje Zeljko (hrvoje.zeljko) <hrvoje.zeljko@e-ito.hr>
2017-08-11 09:59:33 +0200

    Password for User user changed to user

--- Admin/filesystem/mysql_data/user_management/sql_scripts/import_demodata.sql
+++ Admin/filesystem/mysql_data/user_management/sql_scripts/import_demodata.sql
@@ -10,7 +10,7 @@ delete from profile_menu;
 
 insert into user (id, username, password, first_name, last_name, email) values 
     (1, 'admin', '$argon2i$v=19$m=65536,t=2,p=1$8buuloxG+y4CecxZjlKhPw$UDAi/srpZrmgCOny30uhGcMnBJYgBQFECvGB9H/GA44', 'John', 'Admin', 'john.admin@e-ito.de')
-    ,(2, 'user', '$argon2i$v=19$m=65536,t=2,p=1$8buuloxG+y4CecxZjlKhPw$UDAi/srpZrmgCOny30uhGcMnBJYgBQFECvGB9H/GA44', 'Jack', 'User', 'jack.user@e-ito.de');
+    ,(2, 'user', '$argon2i$v=19$m=65536,t=2,p=1$Bzf5J8QwlC/h+HPqJ2np9Q$BRdbtvsoXDQlTEknHSfCs6h+p5A5OV+wq2xwQPEecSk', 'Jack', 'User', 'jack.user@e-ito.de');
     
 insert into user_group (id, name, description) values 
     (1, 'Administrators', 'Description for user group administrators')
