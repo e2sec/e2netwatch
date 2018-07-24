766752ec34be4fdc7ebb0acaaf50b9004075196c
Hrvoje Zeljko <h.zeljko@e2security.de>
2018-07-24 12:04:36 +0000

    Encrypted password for DB user root changed

--- Admin/filesystem/mysql_data/sql_scripts/import_access.sql
+++ Admin/filesystem/mysql_data/sql_scripts/import_access.sql
@@ -1,6 +1,6 @@
 -- Set password on root user on all hosts
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password AS '*315D050BFD5453A9B221D3E8F2D97262E99ADF53';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password AS '*315D050BFD5453A9B221D3E8F2D97262E99ADF53';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password AS '*358D2E69BAE0D45CF5FD2374EE03AC44DFB05182';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password AS '*358D2E69BAE0D45CF5FD2374EE03AC44DFB05182';
 
 -- Create new user middleware, if exists - clear first
 DROP USER IF EXISTS 'middleware'@'%';
