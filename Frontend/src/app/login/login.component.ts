d697d9fbe3c9a69fba9435dd9060a51c2617a1b8
Nikola Milicic <nikola.milicic991@gmail.com>
2018-08-08 10:13:05 +0200

    Login form validation

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'e2nw-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm: FormGroup;
+  submitted = false;
   constructor() { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+
   }
 
 }
