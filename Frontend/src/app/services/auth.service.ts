affa46818d73079c2f26614967959d08ea97f19a
Nikola Milicic <nikola.milicic991@gmail.com>
2018-09-11 13:15:30 +0200

    Removed user-profile service

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { LoginModel } from '../models/login-model';
-import { Router } from '../../../node_modules/@angular/router';
 import { Observable } from '../../../node_modules/rxjs';
-import { UserProfileService } from './user-profile.service';
-import { UserProfile } from '../models/user-profile';
 import * as jwt_decode from 'jwt-decode';
 import { environment } from '../../environments/environment';
 
@@ -14,7 +10,7 @@ import { environment } from '../../environments/environment';
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient, private router: Router, private userProfileService: UserProfileService) { }
+  constructor(private httpClient: HttpClient) { }
 
   login(loginData: LoginModel): Observable<any> {
     return this.httpClient.post<LoginModel>(`${environment.restApiUrl}middleware/api/auth/login`, loginData);
