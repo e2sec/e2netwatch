f651e964d9d63bc8d9f20edcc27afdec680d3fb6
Sergey Makarov <s.makarov@e2security.de>
2018-11-02 22:43:56 +0100

    adding doc comments to KafkaConsumerMaster class; removing auto.commit.interval from configuration as auto.commit set to false -> manual commit by consumer itself

--- nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/kafka/KafkaConsumerMaster.java
+++ nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/kafka/KafkaConsumerMaster.java
@@ -6,10 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,31 +16,56 @@ import org.apache.kafka.common.PartitionInfo;
 
 import com.espertech.esper.client.EPServiceProvider;
 
+/**
+ * KafkaConsumerMaster class has to delegate job for multi-threaded KafkaConsumers [class: KafkaConsumerCallable] ; 
+ * Each Consumer should be assigned to its own group, which in turn consists of multiple threads with its own instance of KafkaConsumerCallable;
+ * Number of threads equals number of partitions for particular topic (by default in kafka ONE for each topic);
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class KafkaConsumerMaster {
 
 	private List<String> topics;
 	private EPServiceProvider engine;
-	private List<Callable<Integer>> consumerThreads;
 	private KafkaConsumer consumerGeneral;
 	private Properties configIn;
 	private Map<String,ExecutorService> groupId_executor;
-	private Map<String,KafkaConsumerCallable> topic_consumer;
-
-	/*
-	 * Per Group: multiple threads of KafkaConsumer for particular topic;
+	
+	/**
+	 * @param engine ONE Esper Engine for each groupID 
+	 * Reason: multithreading by Engine itself with really good throughput for one instance (up to 500.000 events)
+	 * @see http://esper.espertech.com/release-7.0.0/esper-reference/html_single/index.html#api-threading-advanced
+	 * @see http://esper.espertech.com/release-7.0.0/esper-reference/html_single/index.html#perf-tips-3-a
 	 */
 	public KafkaConsumerMaster(EPServiceProvider engine) {
-		this.groupId_executor = new HashMap<>();
-		this.topic_consumer = new HashMap<>();
-		this.configIn = new Properties();
-		this.consumerThreads = new ArrayList<>();
 		this.engine = engine;
 	}
 	
+	/**
+	 * @param config Configuration used by each KafkaConsumerCallable
+	 * @see https://kafka.apache.org/11/documentation.html#newconsumerconfigs
+	 *  bootstrap.servers: list of host/port to use for establishing the initial connection. 
+	 *  				   The name of docker container used (e.g. kafka). 
+	 *  				   For dev you can specify localhost for your local instance;
+	 *  enable.auto.commit: if true -> the consumer's offset will be periodically committed in the background.
+	 *  					Set to false due to manual commit handling by KafkaConsumerCallable.
+	 *  				    It will be committed only if all the records from ConsumerRecords collection were sent to Esper
+	 *  					see class KafkaConsumerCallable, call() method;
+	 * 	group.id: specifies the name of the consumer group a kafka consumer belongs to
+	 *  		  Generated as follows within startWorkers() method:
+	 *  		  group.id.prefix from custom configuration (application.properties) concatenated with the name of topic 
+	 *  		  as we group consumers within one group regarding specific topic;
+	 *  client.id: to be able to track the source of requests beyond just ip/port by allowing a logical application name
+	 *  		   to be included in server-side request logging;
+	 *  		   Can be used for debugging of Kafka Server;
+	 *  		   Generated as follows: concatenation of custom consumer.client.id + # of Thread (per partition). 
+	 *  deserializer: can be custom classes;
+	 * @return itself in order to be instantiated in methods chain
+	 */
 	public KafkaConsumerMaster startWorkers(Properties config) {
+		this.groupId_executor = new HashMap<>();
+		this.configIn = new Properties();
 		configIn.put("bootstrap.servers", config.get("bootstrap.servers"));
 		configIn.put("enable.auto.commit", config.get("enable.auto.commit"));
-		configIn.put("auto.commit.interval.ms", config.get("auto.commit.interval.ms"));
 		configIn.put("session.timeout.ms", config.get("session.timeout.ms"));
 		configIn.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		configIn.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -50,24 +73,19 @@ public class KafkaConsumerMaster {
 		topics.forEach(topic -> {
 			int partitionCount = getPartionsNumber(topic);
 			ExecutorService exec = Executors.newFixedThreadPool(partitionCount);
-			String groupId = configIn.get("consumer.group.id.prefix") + topic;
+			String groupId = config.get("consumer.group.id.prefix") + topic;
 			configIn.put("group.id", groupId);
 			this.groupId_executor.put(groupId, exec);
 			IntStream.rangeClosed(1, partitionCount).forEach(partitionCounter -> {
-				configIn.put("client.id", config.get("consumer.client.id") + String.valueOf(partitionCounter));
+				configIn.put("client.id", config.get("consumer.client.id") + "_Thread #" + String.valueOf(partitionCounter));
 				KafkaConsumerCallable consumer = new KafkaConsumerCallable<>(configIn, topic, engine);
-				consumerThreads.add(null);
-				topic_consumer.put(topic, null);
-				Future<Integer> future = exec.submit(consumer);
+				exec.submit(consumer);
 			});
 		});
 		return this;
 	}
 
-	public List<String> getKafkaGroups() {
-		return groupId_executor.keySet().stream().collect(Collectors.toList());
-	}
-
+	//TODO: test whether the number of partitions can be read correctly from kafka with the following custom method:
 	private int getPartionsNumber(String topic) {
 		int count;
 		consumerGeneral = new KafkaConsumer<>(configIn);
@@ -79,6 +97,15 @@ public class KafkaConsumerMaster {
 		}
 		consumerGeneral.close();
 		return count;
+	}
+	
+	/**
+	 * implementing shutdown for each thread;
+	 * threads are grouped by Executor with Thread Pool regarding each kafka group in groupId_executor Map
+	 */
+	
+	public List<String> getKafkaGroups() {
+		return groupId_executor.keySet().stream().collect(Collectors.toList());
 	}
 	
 	public void closeThreads(String groupId) {
