a895345247e3597f546fc987930af3d6175a9604
Sergey Makarov <s.makarov@e2security.de>
2018-11-02 16:08:33 +0100

    adding master/manager consumer to delegate consumer workers each as standalone thread in sence of multithreading orchestration

--- nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/KafkaConsumerMaster.java
+++ nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/KafkaConsumerMaster.java
@@ -1,106 +1,85 @@
 package de.e2security.netflow_flowaggregation;
 
 import java.io.Serializable;
-import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.kafka.common.PartitionInfo;
 
 import com.espertech.esper.client.EPServiceProvider;
 
-public class CustomKafkaConsumer<K extends Serializable, V extends Serializable> implements Callable<Integer> {
-	private static final Duration duration = Duration.ofSeconds(1L);
-	private static final Logger LOG = LoggerFactory.getLogger(CustomKafkaConsumer.class);
+public class KafkaConsumerMaster {
 
-	private String clientId;
-	private KafkaConsumer<K, V> consumer;
-	private String topic;
+	private List<String> topics;
 	private EPServiceProvider engine;
+	private List<Callable<Integer>> consumerThreads;
+	private KafkaConsumer consumerGeneral;
+	private Properties configIn;
+	private Map<String,ExecutorService> groupId_executor;
+	private Map<String,KafkaConsumerCallable<String, String>> topic_consumer;
 
-	private AtomicBoolean closed = new AtomicBoolean();
-
-	public CustomKafkaConsumer(Properties config, EPServiceProvider engine) {
-		Properties configIn = new Properties();
+	/*
+	 * Per Group: multiple threads of KafkaConsumer for particular topic;
+	 */
+	public KafkaConsumerMaster(Properties config, EPServiceProvider engine) {
+		this.groupId_executor = new HashMap<>();
+		this.topic_consumer = new HashMap<>();
+		configIn = new Properties();
+		consumerThreads = new ArrayList<>();
 		configIn.put("bootstrap.servers", config.get("bootstrap.servers"));
 		configIn.put("enable.auto.commit", config.get("enable.auto.commit"));
 		configIn.put("auto.commit.interval.ms", config.get("auto.commit.interval.ms"));
 		configIn.put("session.timeout.ms", config.get("session.timeout.ms"));
-		configIn.put("client.id", config.get("consumer.client.id"));
-		configIn.put("group.id", config.get("consumer.group.id"));
 		configIn.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		configIn.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		this.clientId = config.getProperty(ConsumerConfig.CLIENT_ID_CONFIG);
-		this.topic = config.getProperty("consumer.topic");
+		this.topics = new ArrayList<String>(Arrays.asList(config.getProperty("consumer.topics").split(",")));
 		this.engine = engine;
-		this.consumer = new KafkaConsumer<>(configIn);
-	}
-
-	@Override
-	public Integer call() throws Exception {
-
-		try {
-			LOG.info("Starting the Consumer: {}", clientId);
-			synchronized (consumer) {
-				consumer.subscribe(Arrays.asList(topic)); //implied seek to the last 'commited position'
+		for (String topic : topics) {
+			int partitionCount = getPartionsNumber(topic);
+			ExecutorService exec = Executors.newFixedThreadPool(partitionCount);
+			String groupId = config.get("consumer.group.id.prefix") + topic;
+			configIn.put("group.id", groupId);
+			this.groupId_executor.put(groupId, exec);
+			for (int i=1; i<=partitionCount;i++) {
+				configIn.put("client.id", config.get("consumer.client.id") + String.valueOf(i));
+				KafkaConsumerCallable<String, String> consumer = new KafkaConsumerCallable<>(configIn, topic, engine);
+				consumerThreads.add(null);
+				topic_consumer.put(topic, null);
+				Future<Integer> future = exec.submit(consumer);
 			}
-			
-			LOG.info("C: {}, Started to process records for topic : {}", clientId, topic);
-
-			ConsumerRecords<K, V> records;
-			while (!closed.get()) {
-				if (Thread.currentThread().isInterrupted()) {
-					closed.set(true);
-				} else {
-					synchronized (consumer) {
-						records = consumer.poll(duration);
-					}
-
-					if (records.isEmpty()) {
-						LOG.debug("C: {}, Found no records", clientId);
-						continue;
-					}
-					try {
-						LOG.info("C: {} Total No. of records received: {}", clientId, records.count());
-						for (ConsumerRecord<K, V> record : records) {
-							LOG.debug("C: {}, Record received topic: {}, partition: {}, key: {}, value: {}, offset: {}",
-									clientId, record.topic(), record.partition(), record.key(), record.value(),
-									record.offset());
-							NetflowEvent netflowEvent = new NetflowEvent(record.value().toString());
-							engine.getEPRuntime().sendEvent(netflowEvent);
-						}
-						consumer.commitAsync();
-					} catch (NetflowEventException e) {
-						LOG.error("No Esper event created. No commit for the recent ConsumerRecords has been done!");
-					}
-				}
-			}
-		} catch (org.apache.kafka.common.errors.InterruptException e) {
-			LOG.info("C: {}, consumer interupted", clientId);
-		} catch (Exception e) {
-			LOG.error("Error while consuming messages", e);
-			throw new Exception(e);
-		} finally {
-			LOG.info("C: {}, consumer exited", clientId);
-			consumer.close(duration);
 		}
-
-		return 0;
 	}
 
-	public void commit() {
-		consumer.commitSync();
+	public List<String> getKafkaGroups() {
+		return groupId_executor.keySet().stream().collect(Collectors.toList());
 	}
 
-	public void close() {
-		closed.set(true);
+	private int getPartionsNumber(String topic) {
+		int count;
+		consumerGeneral = new KafkaConsumer<>(configIn);
+		Map<String, List<PartitionInfo>> listTopics = (Map<String, List<PartitionInfo>>) consumerGeneral.listTopics();
+		if (listTopics.isEmpty()) {
+			throw new RuntimeException("No Topics are available to read from Kafka -> no any KafkaConsumer can be created");
+		} else {
+			count = listTopics.get(topic).size();
+		}
+		consumerGeneral.close();
+		return count;
+	}
+	
+	public void closeThreadsFor(String groupId) {
+		this.groupId_executor.get(groupId).shutdown();
 	}
 
 }
