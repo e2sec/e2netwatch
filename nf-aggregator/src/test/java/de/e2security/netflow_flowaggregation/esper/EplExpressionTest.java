28d7b7e00d1aa449ed5326dde50aa73a57747465
Tobias Nieberg <t.nieberg@e2security.de>
2018-11-09 10:20:43 +0100

    testing correct order of netflow events: TcpEplExpression: eplSortByLastSwitched

--- nf-aggregator/src/test/java/de/e2security/netflow_flowaggregation/esper/EplExpressionTest.java
+++ nf-aggregator/src/test/java/de/e2security/netflow_flowaggregation/esper/EplExpressionTest.java
@@ -1,62 +1,69 @@
 package de.e2security.netflow_flowaggregation.esper;
 
+import java.text.ParseException;
+import java.time.Instant;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.Queue;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.time.CurrentTimeEvent;
+import com.espertech.esper.client.time.CurrentTimeSpanEvent;
 
 import de.e2security.netflow_flowaggregation.netflow.NetflowEvent;
 import de.e2security.netflow_flowaggregation.netflow.NetflowEventOrdered;
-import de.e2security.netflow_flowaggregation.utils.EsperUtil;
 import de.e2security.netflow_flowaggregation.utils.TestUtil;
 import org.junit.Assert;
 
 
 public class EplExpressionTest {
 
-	EPServiceProvider engine;
-	List<String> nfGenSampleDataFromKafka;
-
-	@Before
-	public void init() {
-		this.engine = EsperUtil.registerEvents(NetflowEvent.class, NetflowEventOrdered.class);
-		this.nfGenSampleDataFromKafka = TestUtil.readSampleDataFile("nf_gen.output.sample");
+	private EPServiceProvider engine;
+	
+	@Before public void init() {
+		Configuration config = new Configuration();
+		config.addEventType(NetflowEvent.class);
+		config.addEventType(NetflowEventOrdered.class);
+		config.getEngineDefaults().getThreading().setInternalTimerEnabled(false);
+		engine = EPServiceProviderManager.getDefaultProvider(config);
 	}
 	
-	@After
-	public void stop() {
-		this.engine.destroy();
-	}
-	
-	@Test public void eventGettingIntoCorrectTimeOrder() {
-		engine.getEPAdministrator().createEPL(TcpEplExpressions.eplSortByLastSwitched());
-		EPStatement statement = engine.getEPAdministrator().createEPL(TcpEplExpressionsTest.selectNetStreamOrdered());
-		NetflowEventsCorrectOrderTestListener testListener = new NetflowEventsCorrectOrderTestListener(false);
-		statement.addListener(testListener);
-		EsperTestUtil.sendDataToEsper(nfGenSampleDataFromKafka, engine);
-		Queue<ZonedDateTime> dates = testListener.getDates();
-		Assert.assertEquals(nfGenSampleDataFromKafka.size(), dates.size());
-		List<Boolean> assertions = new ArrayList<>(); //assertion: the same number of successively sequenced data regarding their date time
+	@Test public void test() throws ParseException {
+		List<NetflowEvent> events = EsperTestUtil.getHistoricalEvents(TestUtil.readSampleDataFile("nf_gen.output.sample"), 100);
+		NetflowEventsCorrectOrderTestListener listener = new NetflowEventsCorrectOrderTestListener(true);
+		DateTimeFormatter formater = DateTimeFormatter.ISO_INSTANT;
+		ZonedDateTime initial = events.get(0).getLast_switched();
+		Date startTime = Date.from(Instant.from(formater.parse(initial.toString()))); //get initial time point for window
+		/*
+		 * setting listener on the second statement;
+		 * however, through adding rstream into the first statement after keyword 'select' the same result can be achieved w/o the second one;
+		 */
+		EPStatement statement1 = engine.getEPAdministrator().createEPL(TcpEplExpressions.eplSortByLastSwitched());
+		EPStatement statement2 = engine.getEPAdministrator().createEPL(TcpEplExpressionsTest.selectNetStreamOrdered());
+		statement2.addListener(listener);
+		engine.getEPRuntime().sendEvent(new CurrentTimeEvent(startTime.getTime())); // set the initial start window for the historical data (external timer)
+		events.forEach(event -> {
+			engine.getEPRuntime().sendEvent(event);
+		});
+		engine.getEPRuntime().sendEvent(new CurrentTimeSpanEvent(startTime.getTime() + 10*60*1000, 100)); //set advance time for 10 minutes while sliding window each 100 ms;
+		Queue<ZonedDateTime> dates = listener.getDates();
+		int correctOrder = 0;
 		while (dates.size() >= 2) {
-			ZonedDateTime theMostLatestDate = dates.poll(); // polling from the head: the most recent/latest data due to FIFO and ascending order
-			ZonedDateTime dateBeforeTheMostLatestDate = dates.peek();
-			if (dateBeforeTheMostLatestDate.compareTo(theMostLatestDate) <= 0) //compareTo results: negative int if this < that; 0 if this == that; positive int if this > that
-			{
-				assertions.add(true);
-			}
-			else
-			{
-				assertions.add(false);
+			ZonedDateTime lessRecent = dates.poll();
+			ZonedDateTime moreRecent = dates.peek(); 
+			if (lessRecent.compareTo(moreRecent) == 0 || lessRecent.compareTo(moreRecent) < 0) { //this < that = negative, this > that = positive, 0 = neutral
+				correctOrder++;
 			}
 		}
-		Assert.assertEquals(nfGenSampleDataFromKafka.size() - 3, assertions.size()); 
+		Assert.assertEquals(events.size() - 1, correctOrder);
 	}
 	
 }
--- nf-aggregator/src/test/java/de/e2security/netflow_flowaggregation/esper/EsperTestUtil.java
+++ nf-aggregator/src/test/java/de/e2security/netflow_flowaggregation/esper/EsperTestUtil.java
@@ -1,21 +1,41 @@
 package de.e2security.netflow_flowaggregation.esper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.time.CurrentTimeEvent;
 
 import de.e2security.netflow_flowaggregation.netflow.NetflowEvent;
 
 public final class EsperTestUtil {
-	
+
 	//sending events to EPEngine (mocking up kafka consumer)
 	public static void sendDataToEsper(List<String> lines, EPServiceProvider engine) {
 		lines.forEach(line -> {
 			NetflowEvent event = null;
 			try { event = new NetflowEvent(line); } 
 			catch (Exception e) { e.printStackTrace(); }
+			engine.getEPRuntime().sendEvent(new CurrentTimeEvent(1541249347));
 			engine.getEPRuntime().sendEvent(event);
 		});
 	}
-	
+
+	//specify the number of events to be tested, taking control over sending the data during tests
+	public static List<NetflowEvent> getHistoricalEvents(List<String> lines, int quantity) {
+		List<NetflowEvent> testEvents = new ArrayList<>();
+		int counter = 1;
+		for(String line : lines) {
+			try { 
+				NetflowEvent event = new NetflowEvent(line);
+				testEvents.add(event);
+			} catch (Exception ex) { ex.printStackTrace(); }
+			if (counter >= quantity) 
+				break;
+			else 
+				counter++;
+		}
+		return testEvents;
+	}
+
 }
