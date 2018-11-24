81db0256b590dd9a0dc51628641aeeedeefbe6fe
Sergey Makarov <s.makarov@e2security.de>
2018-11-24 17:34:58 +0100

    adding comments regarding not implemented schema fields in ecs version 0.1.0. The names are taken as suggestion by feauture's reporter

--- nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/utils/Ecs.java
+++ nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/utils/Ecs.java
@@ -12,16 +12,16 @@ public final class Ecs {
 			IPV4_DST_ADDR("destination.ip"), 										
 			L4_DST_PORT("destination.port"),									
 			PROTOCOL("network.iana_number"),								
-			IN_FLOW_SEQ_NUM("netflow.inbound.sequence_number"), 	
-			IN_FLOW_RECORDS("netflow.inbound.records"),						
-			OUT_FLOW_SEQ_NUM("netflow.outbound.sequence_number"),	
-			OUT_FLOW_RECORDS("netflow.outbound.records"),					
+			IN_FLOW_SEQ_NUM("netflow.inbound.sequence_number"), //custom impl. -> not in 0.1.0 ecs.version yet 	
+			IN_FLOW_RECORDS("netflow.inbound.records"),	//custom impl. -> not in 0.1.0 ecs.version yet
+			OUT_FLOW_SEQ_NUM("netflow.outbound.sequence_number"), //custom impl. -> not in 0.1.0 ecs.version yet
+			OUT_FLOW_RECORDS("netflow.outbound.records"), //custom impl. -> not in 0.1.0 ecs.version yet					
 			IN_BYTES("network.inbound.bytes"),							
 			OUT_BYTES("network.outbound.bytes"),						
 			IN_PKTS("network.inbound.packets"),						
 			OUT_PKTS("network.outbound.packets"),					
-			IN_TCP_FLAGS("network.inbound.tcp_flags"),					
-			OUT_TCP_FLAGS("network.outbound.tcp_flags"),				
+			IN_TCP_FLAGS("network.inbound.tcp_flags"), //custom impl. -> not in 0.1.0 ecs.version yet					
+			OUT_TCP_FLAGS("network.outbound.tcp_flags"), //custom impl. -> not in 0.1.0 ecs.version yet				
 			IN_FIRST_SWITCHED("netflow.inbound.first_switched"),		
 			OUT_FIRST_SWITCHED("netflow.outbound.first_switched"), 	
 			IN_LAST_SWITCHED("netflow.inbound.last_switched"),			
