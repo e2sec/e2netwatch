97ec6fbb0c4ea870851bfb2a6653bdbf23f23105
Sergey Makarov <s.makarov@e2security.de>
2018-11-29 22:10:52 +0100

    implementing traffic direction algorithm based on ports' distinction

--- nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/esper/ProtocolRegisterTrigger.java
+++ nf-aggregator/src/main/java/de/e2security/netflow_flowaggregation/esper/ProtocolRegisterTrigger.java
@@ -1,10 +1,29 @@
 package de.e2security.netflow_flowaggregation.esper;
 
-import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.*;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.HOST;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IN_BYTES;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IN_FIRST_SWITCHED;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IN_FLOW_RECORDS;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IN_FLOW_SEQ_NUM;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IN_LAST_SWITCHED;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IN_PKTS;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IPV4_DST_ADDR;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.IPV4_SRC_ADDR;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.L4_DST_PORT;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.L4_SRC_PORT;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.OUT_BYTES;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.OUT_FIRST_SWITCHED;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.OUT_FLOW_RECORDS;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.OUT_FLOW_SEQ_NUM;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.OUT_LAST_SWITCHED;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.OUT_PKTS;
+import static de.e2security.netflow_flowaggregation.utils.Ecs.Schema.Netflow.PROTOCOL;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,31 +41,71 @@ public class ProtocolRegisterTrigger implements UpdateListener {
 	public ProtocolRegisterTrigger(CustomKafkaProducer<Serializable, Serializable> producer) {
 		this.producer = producer;
 	}
+	
+	private static class Host {
+		private String addr;
+		private Integer port;
+		Host(String addr, Integer port) {
+			this.addr = addr;
+			this.port = port;
+		}
+		String getAddr() { return this.addr; }
+		Integer getPort() { return this.port; }
+	}
+	
+	//NW-76 distinguish traffic direction by port numbers.
+	private Pair<Host,Host> sortByLowestPort(ProtocolRegister prtcl) {
+		String srcaddr = prtcl.getIn_ipv4_src_addr();
+		Integer srcport = prtcl.getIn_l4_src_port();
+		String dstaddr = prtcl.getOut_ipv4_src_addr();
+		Integer dstport = prtcl.getOut_14_src_port();
+		ZonedDateTime in_last_switched = prtcl.getIn_last_switched();
+		ZonedDateTime out_last_switched = prtcl.getOut_last_switched();
+		
+		Host src = new Host(srcaddr, srcport);
+		Host dst = new Host(dstaddr, dstport);
+		
+		//higher port = source host; lower port = dst hosts
+		int switchCmp = in_last_switched.compareTo(out_last_switched);	
+		if (switchCmp == 0) {
+			int portCmp = srcport.compareTo(dstport);
+			//exchange only in the following case
+			if (portCmp < 0) {
+				dst = new Host(srcaddr,srcport);
+				src = new Host(dstaddr,dstport);
+			} 
+		}
+		return new ImmutablePair<>(src, dst);
+	}
 
 	@Override
 	public void update(EventBean[] newData, EventBean[] oldEvents) {
 		JSONObject jsonObject = new JSONObject();
 		try {
-			ProtocolRegister proto = (ProtocolRegister) newData[0].getUnderlying();
-			String description = proto.getDescription();
-			String host = proto.getIn_host();
-			String srcaddr = proto.getIn_ipv4_src_addr();
-			Integer srcport = proto.getIn_l4_src_port();
-			String dstaddr = proto.getIn_ipv4_dst_addr();
-			Integer dstport = proto.getIn_l4_dst_port();
-			Integer protocol = proto.getProtocol();
-			Integer in_flow_seq_num = proto.getIn_flow_seq_num();
-			Integer in_flow_records = proto.getIn_flow_records();
-			Integer out_flow_seq_num = proto.getOut_flow_seq_num();
-			Integer out_flow_records = proto.getOut_flow_records();
-			Integer in_bytes = proto.getIn_bytes();
-			Integer out_bytes = proto.getOut_bytes();
-			Integer in_pkts = proto.getIn_pkts();
-			Integer out_pkts = proto.getOut_pkts();
-			ZonedDateTime in_first_switched = proto.getIn_first_switched();
-			ZonedDateTime out_first_switched = proto.getOut_first_switched();
-			ZonedDateTime in_last_switched = proto.getIn_last_switched();
-			ZonedDateTime out_last_switched = proto.getOut_last_switched();
+			ProtocolRegister prtcl = (ProtocolRegister) newData[0].getUnderlying();
+			Pair<Host,Host> pair = sortByLowestPort(prtcl);
+			
+			String srcaddr = pair.getLeft().getAddr();
+			Integer srcport = pair.getLeft().getPort();
+			String dstaddr = pair.getRight().getAddr();
+			Integer dstport = pair.getRight().getPort();	
+			
+			String description = prtcl.getDescription();
+			String host = prtcl.getIn_host();
+			Integer protocol = prtcl.getProtocol();
+			Integer in_flow_seq_num = prtcl.getIn_flow_seq_num();
+			Integer in_flow_records = prtcl.getIn_flow_records();
+			Integer out_flow_seq_num = prtcl.getOut_flow_seq_num();
+			Integer out_flow_records = prtcl.getOut_flow_records();
+			Integer in_bytes = prtcl.getIn_bytes();
+			Integer out_bytes = prtcl.getOut_bytes();
+			Integer in_pkts = prtcl.getIn_pkts();
+			Integer out_pkts = prtcl.getOut_pkts();
+			ZonedDateTime in_last_switched = prtcl.getIn_last_switched();
+			ZonedDateTime out_last_switched = prtcl.getOut_last_switched();
+			ZonedDateTime in_first_switched = prtcl.getIn_first_switched();
+			ZonedDateTime out_first_switched = prtcl.getOut_first_switched();
+			
 			LOG.info(String.format("%s Connection %s:%d -> %s:%d (%d/%d Bytes)", 
 					description, srcaddr, srcport, dstaddr, dstport, in_bytes, out_bytes));
 			//KEY names are compatible to ECS SCHEMA. ecs.version 0.1.0
