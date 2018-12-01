b5983ca3ce169759b46e66b157ea0ee7e343584b
Sergey Makarov <s.makarov@e2security.de>
2018-12-01 10:39:39 +0100

    silence all tests that had already passed

--- nf-aggregator/src/test/java/nw76/TestTrafficRecognitionByPortDistinction.java
+++ nf-aggregator/src/test/java/nw76/TestTrafficRecognitionByPortDistinction.java
@@ -11,16 +11,17 @@ import de.e2security.netflow_flowaggregation.esper.ProtocolRegisterTrigger.Host;
 import de.e2security.netflow_flowaggregation.model.protocols.ProtocolRegister;
 
 public class TestTrafficRecognitionByPortDistinction {
-	@Test public void higherPortAsSourceHostTest() {
-		 //higher port = source host; lower port = dst hosts
+	//higher port = source host; lower port = dst hosts
+	@Test public void higherPortAsSourceHostTestWithTheSameTimestamp() {
 		//1. src_port is higher as dst_port
+		ZonedDateTime now = ZonedDateTime.now();
 		ProtocolRegister prtcl = new ProtocolRegister();
 		prtcl.setIn_l4_src_port(7654);
 		prtcl.setIn_l4_dst_port(4567);
 		prtcl.setOut_14_src_port(4567);
 		prtcl.setOut_14_dst_port(7654);
-		prtcl.setIn_last_switched(ZonedDateTime.now());
-		prtcl.setOut_last_switched(ZonedDateTime.now());
+		prtcl.setIn_last_switched(now);
+		prtcl.setOut_last_switched(now);
 		ProtocolRegisterTrigger trigger = new ProtocolRegisterTrigger(null);
 		Pair<Host,Host> src_dst = trigger.defineByPort(prtcl);
 		Assert.assertEquals(src_dst.getLeft().getPort(), (Integer) 7654);
@@ -31,11 +32,28 @@ public class TestTrafficRecognitionByPortDistinction {
 		prtcl2.setIn_l4_dst_port(9191);
 		prtcl2.setOut_14_src_port(9191);
 		prtcl2.setOut_14_dst_port(8080);
-		prtcl2.setIn_last_switched(ZonedDateTime.now());
-		prtcl2.setOut_last_switched(ZonedDateTime.now());
+		prtcl2.setIn_last_switched(now);
+		prtcl2.setOut_last_switched(now);
 		ProtocolRegisterTrigger trigger2 = new ProtocolRegisterTrigger(null);
 		Pair<Host,Host> src_dst2 = trigger2.defineByPort(prtcl2);
 		Assert.assertEquals(src_dst2.getLeft().getPort(), (Integer) 9191);
 		Assert.assertEquals(src_dst2.getRight().getPort(),(Integer) 8080);
+	}
+	
+	@Test public void noHostExchangeIfTimestampNotTheSame() {
+		ZonedDateTime before = ZonedDateTime.now();
+		ZonedDateTime after = before.plusMinutes(1L);
+		ProtocolRegister prtcl = new ProtocolRegister();
+		prtcl.setIn_l4_src_port(3321);
+		prtcl.setIn_l4_dst_port(5353);
+		prtcl.setOut_14_src_port(5353);
+		prtcl.setOut_14_dst_port(3321);
+		prtcl.setIn_last_switched(before);
+		prtcl.setOut_last_switched(after);
+		ProtocolRegisterTrigger trigger = new ProtocolRegisterTrigger(null);
+		Pair<Host,Host> src_dst = trigger.defineByPort(prtcl);
+		//assert the higher port will be not the source host, since timestamp differs and Esper sort events within window properly
+		Assert.assertEquals(src_dst.getLeft().getPort(),(Integer) 3321);
+		Assert.assertEquals(src_dst.getRight().getPort(),(Integer) 5353);
 	}
 }
